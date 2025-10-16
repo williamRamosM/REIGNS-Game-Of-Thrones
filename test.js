a28d69dbf875f324b84756dbc3092c42ff745091
williamRamosM <williamramosmoreira01@gmail.com>
2025-10-16 14:33:00 -0300

    atualizado test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 let cont = 0;
-
+let numImg = 0;
 const imgs = [
     "../diversos/imagens/dia.jpg"
 ]
@@ -10,8 +10,15 @@ botao.addEventListener("click", acaon)
 function acaon(){
 
     cont++
-    if(){
-
+    if(cont == 1){
+        numImg = 0
     }
-    body.style.backgroundImage = `url('${imgs[0]}')`
+    else if(cont == 2){
+        numImg = 1
+    }
+    else{
+        numImg = 2
+        cont = 0;
+    }
+    body.style.backgroundImage = `url('${imgs[numImg]}')`
 }
