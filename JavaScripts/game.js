df7c0dc4d4c1db3a84cfb99f53ba8612946745d6
JOSÉ BERNARDO DA LUZ CARDOZO <2023326213@ALUNOS.OSORIO.IFRS.EDU.BR>
2025-11-13 15:06:33 -0300

    Objeto dos personagens criados, além do fundo de dungeon adicionado

---
+++ JavaScripts/game.js
@@ -0,0 +1,159 @@
+const espirito = {
+    nome: "Espírito dos Caídos",
+    foto: "../diversos/imagens/Espirito.png",
+    fundo: "../diversos/imagens/Vilarejo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const arcebispo = {
+    nome: "Arcebispo",
+    foto: "../diversos/imagens/Arcebispo.png",
+    fundo: "../diversos/imagens/Vilarejo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const general = {
+    nome: "General Conventon",
+    foto: "../diversos/imagens/General.png",
+    fundo: "../diversos/imagens/Castelo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const freira = {
+    nome: "Irmâ Júlia",
+    foto: "../diversos/imagens/Julia.png",
+    fundo: "../diversos/imagens/Vilarejo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const rex = {
+    nome: "Rex",
+    foto: "../diversos/imagens/Rex.png",
+    fundo: "../diversos/imagens/Castelo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const conselheiro = {
+    nome: "Senhor Gotfried",
+    foto: "../diversos/imagens/Gotfried.png",
+    fundo: "../diversos/imagens/Castelo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const lula = {
+    nome: "Luís do Povo",
+    foto: "../diversos/imagens/Luis.png",
+    fundo: "../diversos/imagens/Vilarejo.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const diabo = {
+    nome: "Briggs",
+    foto: "../diversos/imagens/Briggs.png",
+    fundo: "../diversos/imagens/demonio.png",
+    musica: "../diversos/audios/Castelo.mp3"
+}
+
+const portaDupla = {
+    nome: "Duas Portas",
+    foto: "../diversos/imagens/DuasPortas.png",
+    fundo: "../diversos/imagens/Dungeon.png",
+    musica: "../diversos/audios/Dungeon.mp3"
+}
+
+const porta = {
+    nome: "Porta",
+    foto: "../diversos/imagens/Porta.png",
+    fundo: "../diversos/imagens/Dungeon.png",
+    musica: "../diversos/audios/Dungeon.mp3"
+}
+
+const salaBau = {
+    nome: "Sala com um baú",
+    foto: "../diversos/imagens/Bau.png",
+    fundo: "../diversos/imagens/Dungeon.png",
+    musica: "../diversos/audios/Dungeon.mp3"
+}
+
+const portaChefe = {
+    nome: "Porta para uma sala com grande ameaça",
+    foto: "../diversos/imagens/PortaDoChefe.png",
+    fundo: "../diversos/imagens/Dungeon.png",
+    musica: "../diversos/audios/Dungeon.mp3"
+}
+
+const portaChefao = {
+    nome: "Porta para encontrar uma lenda antiga",
+    foto: "../diversos/imagens/PortaMaliketh.png",
+    fundo: "../diversos/imagens/Dungeon.png",
+    musica: "../diversos/audios/Dungeon.mp3"
+}
+
+const malenia = {
+    nome: "Malenia, a Lâmina do Satan",
+    foto: "../diversos/imagens/Malenia.png",
+    fundo: "../diversos/imagens/PortaDoChefe.png",
+    musica: "../diversos/audios/Malenia.mp3"
+}
+
+const godfrey = {
+    nome: "Godfrey, o Guerreiro",
+    foto: "../diversos/imagens/Godfrey.png",
+    fundo: "../diversos/imagens/PortaDoChefe.png",
+    musica: "../diversos/audios/Godfrey.mp3"
+}
+
+const morgott = {
+    nome: "Morgott, o Rei das Feras",
+    foto: "../diversos/imagens/Morgott.png",
+    fundo: "../diversos/imagens/PortaDoChefe.png",
+    musica: "../diversos/audios/Morgott.mp3"
+}
+
+const radahn = {
+    nome: "Radahn, o Senhor das Estrelas",
+    foto: "../diversos/imagens/Radahn.png",
+    fundo: "../diversos/imagens/PortaDoChefe.png",
+    musica: "../diversos/audios/Radahn.mp3"
+}
+
+const mohg = {
+    nome: "Mohg, o Lorde do Sangue",
+    foto: "../diversos/imagens/Mohg.png",
+    fundo: "../diversos/imagens/PortaDoChefe.png",
+    musica: "../diversos/audios/Mohg.mp3"
+}
+
+const maliketh = {
+    nome: "Maliketh, a Morte",
+    foto: "../diversos/imagens/Maliketh.png",
+    fundo: "../diversos/imagens/PortaMaliketh.png",
+    musica: "../diversos/audios/Maliketh.mp3"
+}
+
+
+
+
+let ArrayNum2 = [
+
+    espirito,
+    arcebispo,
+    general,
+    freira,
+    rex,
+    conselheiro,
+    lula,
+    diabo,
+    portaDupla,
+    porta,
+    salaBau,
+    portaChefe,
+    portaChefao,
+    malenia,
+    godfrey,
+    morgott,
+    radahn,
+    mohg,
+    maliketh
+    
+]
